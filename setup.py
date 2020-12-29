7c8d547d7dc28057bf7dd503a0746b372464fbe5
Riccardo Gallini <riccardo.gallini@gmail.com>
2020-12-29 23:43:57 +0100

    getProcess with includeGraph

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="ibpm-api-client",
-    version="0.0.1",
+    version="0.0.2",
     description="ibpm web api client",
     url="https://github.com/ibimec/ibpm-api-client",
     author="rgallini",
@@ -17,5 +17,5 @@ setup(
     license="MIT",
     packages=["ibpm"],
     include_package_data=True,
-    install_requires=["requests", "jsons"],
+    install_requires=["requests", "jsons", "pillow"],
 )
