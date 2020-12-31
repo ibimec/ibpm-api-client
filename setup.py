02bdb78eb74777baa182bf41ae2dc9864fef7867
Riccardo Gallini <riccardo.gallini@gmail.com>
2020-12-31 12:22:09 +0100

    released new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="ibpm-api-client",
-    version="0.1.0",
+    version="0.1.1",
     description="ibpm web api client",
     url="https://github.com/ibimec/ibpm-api-client",
     author="rgallini",
